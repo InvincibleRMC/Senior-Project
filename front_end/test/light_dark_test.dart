63a83e3cac4f68729a6f20630ef08e18a81328c1
Michael Carlstrom <rmc@carlstrom.com>
2023-01-22 11:53:06 -0500

    cleaned up tests

--- front_end/test/light_dark_test.dart
+++ front_end/test/light_dark_test.dart
@@ -5,12 +5,12 @@ import 'package:system_theme/system_theme.dart';
 
 void main() {
   group('Light Dark Mode Tests', () {
+    const IconData night = Icons.brightness_2;
+    const IconData light = Icons.wb_sunny;
     testWidgets('Starting State', (WidgetTester tester) async {
       await tester.pumpWidget(const MyApp());
 
       final bool darkMode = SystemTheme.isDarkMode;
-      const IconData night = Icons.brightness_2;
-      const IconData light = Icons.wb_sunny;
 
       // Asserts Correct widget
       if (darkMode) {
@@ -22,14 +22,10 @@ void main() {
     testWidgets('Night to Light', (WidgetTester tester) async {
       await tester.pumpWidget(const MyApp());
 
-      const IconData night = Icons.brightness_2;
-      const IconData light = Icons.wb_sunny;
-
       await tester.tap(find.byIcon(light));
       await tester.pump();
 
       //Asert Night
-
       expect(find.byIcon(night), findsOneWidget);
       expect(find.byIcon(light), findsNothing);
 
@@ -42,9 +38,7 @@ void main() {
     testWidgets('Light to Night', (WidgetTester tester) async {
       await tester.pumpWidget(const MyApp());
 
-      const IconData night = Icons.brightness_2;
-      const IconData light = Icons.wb_sunny;
-
+      //Asert Light
       expect(find.byIcon(night), findsNothing);
       expect(find.byIcon(light), findsOneWidget);
 
