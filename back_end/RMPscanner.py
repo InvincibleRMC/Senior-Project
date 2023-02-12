0ef19530951616f70c9abeb496ed9493e3f5cce4
Michael Carlstrom <rmc170@case.edu>
2023-02-12 14:46:15 -0800

    init

--- back_end/RMPscanner.py
+++ back_end/RMPscanner.py
@@ -5,107 +5,116 @@ from tabulate import tabulate
 data = []
 
 
-def loadProfessorName(URL: str, parsedPage: BeautifulSoup):
+def load_professor_name(URL: str, parsedPage: BeautifulSoup):
     fullName = parsedPage.find("div", class_="NameTitle__Name-dowf0z-0 cfjPUG")
+    if fullName is None:
+        return ["", ""]
+
     try:
         firstName = fullName.text.strip().split()[0] + " " + fullName.text.strip().split()[1]
         lastName = fullName.text.strip().split()[2]
-    except:
+    except IndexError:
         firstName = fullName.text.strip().split()[0]
         lastName = fullName.text.strip().split()[1]
     name = [firstName, lastName]
     return name
 
 
-def loadProfessorScore(URL: str, parsedPage: BeautifulSoup):
+def load_professor_score(URL: str, parsedPage: BeautifulSoup):
     score = parsedPage.find("div", class_="RatingValue__Numerator-qw8sqy-2 liyUjw")
+    if score is None:
+        return ""
     return score.text.strip()
 
 
-def loadProfessorDepartment(URL: str, parsedPage: BeautifulSoup):
+def load_professor_department(URL: str, parsedPage: BeautifulSoup):
     department = parsedPage.find("a", class_="TeacherDepartment__StyledDepartmentLink-fl79e8-0 fuypDB")
+    if department is None:
+        return ""
     return department.text.strip().replace(' department', '')
 
 
-def loadTakeAgainAnDifficulty(URL: str, parsedPage: BeautifulSoup):
-    scores = []
+def load_take_again_and_difficulty(URL: str, parsedPage: BeautifulSoup):
+    scores: list[str] = []
     for i in parsedPage.find_all(attrs={"class": "FeedbackItem__FeedbackNumber-uof32n-1 kkESWs"}):
         scores.append(i.text.strip())
     return scores
 
 
-def loadReviews(URL: str, parsedPage: BeautifulSoup):
+def load_reviews(URL: str, parsedPage: BeautifulSoup):
     reviews = parsedPage.find("li", class_="TeacherRatingTabs__StyledTab-pnmswv-2 bOzrdx react-tabs__tab--selected")
+    if reviews is None:
+        return ""
     return reviews.text.strip().replace(' Student Ratings', '')
 
 
-def addToData(URL: str):
+def add_to_data(URL: str):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
-    name = loadProfessorName(URL, soup)
-    department = loadProfessorDepartment(URL, soup)
-    score = loadProfessorScore(URL, soup)
-    reviews = loadReviews(URL, soup)
-    takeAgainAndDifficulty = loadTakeAgainAnDifficulty(URL, soup)
-    loadTakeAgainAnDifficulty(URL, soup)
-    professorData = name + [department] + [float(score)] + takeAgainAndDifficulty + [reviews]
+    name = load_professor_name(URL, soup)
+    department = load_professor_department(URL, soup)
+    score = load_professor_score(URL, soup)
+    reviews = load_reviews(URL, soup)
+    takeAgainAndDifficulty = load_take_again_and_difficulty(URL, soup)
+    load_take_again_and_difficulty(URL, soup)
+    professorData = name + [department] + [score] + takeAgainAndDifficulty + [reviews]
     data.append(professorData)
 
 
-def convertToSQLInsert():
+def convert_to_SQL_insert():
     pass
 
 
-def convertToSQLUpdate():
+def convert_to_SQL_update():
     pass
 
 
-def printTable():
-    col_names = ["Firt Name", "Last Name", "Department", "RMP Score", "Would Take Again", "Difficulty", "Reviews"]
+def print_table():
+    col_names = ["First Name", "Last Name", "Department", "RMP Score", "Would Take Again", "Difficulty", "Reviews"]
     print(tabulate(data, headers=col_names, tablefmt="fancy_grid", floatfmt=(None, None, None, '.1f', None, '.1f', None)))
 
 
-addToData("https://www.ratemyprofessors.com/professor?tid=2256935")  # Allman,Mark
-addToData("https://www.ratemyprofessors.com/professor?tid=2354474")  # Ayday,Erman
-addToData("https://www.ratemyprofessors.com/professor?tid=2420821")  # Barendt,Nicholas
-addToData("https://www.ratemyprofessors.com/professor?tid=801648")   # Cavusoglu,Cenk
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2256935")  # Allman,Mark
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2354474")  # Ayday,Erman
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2420821")  # Barendt,Nicholas
+add_to_data("https://www.ratemyprofessors.com/professor?tid=801648")   # Cavusoglu,Cenk
 # Chandra,Praphul
 # Chaudhary,Vipin
-addToData("https://www.ratemyprofessors.com/professor?tid=1658282")  # Connamacher,Harold
-addToData("https://www.ratemyprofessors.com/professor?tid=2832423")  # Foreback,Dianne
-addToData("https://www.ratemyprofessors.com/professor?tid=2638814")  # French,Roger
-addToData("https://www.ratemyprofessors.com/professor?tid=2252150")  # Fu,Michael
-addToData("https://www.ratemyprofessors.com/professor?tid=2593133")  # Gajurel,Sanjaya
+add_to_data("https://www.ratemyprofessors.com/professor?tid=1658282")  # Connamacher,Harold
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2832423")  # Foreback,Dianne
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2638814")  # French,Roger
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2252150")  # Fu,Michael
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2593133")  # Gajurel,Sanjaya
 # Ganesan,Ashwin
 # Gupta,Manish
-addToData("https://www.ratemyprofessors.com/professor?tid=1901816")  # Gurkan Cavusoglu,Evren
-addToData("https://www.ratemyprofessors.com/professor?tid=2766350")  # Izadnegahdar,Daniel
-addToData("https://www.ratemyprofessors.com/professor?tid=2849338")  # Jayapandian,Catherine
-addToData("https://www.ratemyprofessors.com/professor?tid=2863682")  # Jimenez Segovia,Luis
-addToData("https://www.ratemyprofessors.com/professor?tid=1104419")  # Koyuturk,Mehmet
-# addToData("https://www.ratemyprofessors.com/professor?tid=2848501") # Kuppannagari,Sanmukh
-addToData("https://www.ratemyprofessors.com/professor?tid=1478435")  # Lewicki,Michael
-addToData("https://www.ratemyprofessors.com/professor?tid=846293")  # Li,Jing
-addToData("https://www.ratemyprofessors.com/professor?tid=2231701")  # Li,Pan
+add_to_data("https://www.ratemyprofessors.com/professor?tid=1901816")  # Gurkan Cavusoglu,Evren
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2766350")  # Izadnegahdar,Daniel
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2849338")  # Jayapandian,Catherine
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2863682")  # Jimenez Segovia,Luis
+add_to_data("https://www.ratemyprofessors.com/professor?tid=1104419")  # Koyuturk,Mehmet
+# add_to_data("https://www.ratemyprofessors.com/professor?tid=2848501") # Kuppannagari,Sanmukh
+add_to_data("https://www.ratemyprofessors.com/professor?tid=1478435")  # Lewicki,Michael
+add_to_data("https://www.ratemyprofessors.com/professor?tid=846293")  # Li,Jing
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2231701")  # Li,Pan
 # Li,Shuo
-addToData("https://www.ratemyprofessors.com/professor?tid=16817")  # Liberatore,Vincenzo
-addToData("https://www.ratemyprofessors.com/professor?tid=2529012")  # Loui,Ronald
+add_to_data("https://www.ratemyprofessors.com/professor?tid=16817")  # Liberatore,Vincenzo
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2529012")  # Loui,Ronald
 # Murthy,Sreerama
-addToData("https://www.ratemyprofessors.com/professor?tid=2699654")  # Omeike,Stanley
-addToData("https://www.ratemyprofessors.com/professor?tid=2571392")  # Ozguner,Orhan
-addToData("https://www.ratemyprofessors.com/professor?tid=16730")  # Podgurski,H
-addToData("https://www.ratemyprofessors.com/professor?tid=846287")  # Rabinovich,Michael
-addToData("https://www.ratemyprofessors.com/professor?tid=1868954")  # Ray,Soumya
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2699654")  # Omeike,Stanley
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2571392")  # Ozguner,Orhan
+add_to_data("https://www.ratemyprofessors.com/professor?tid=16730")  # Podgurski,H
+add_to_data("https://www.ratemyprofessors.com/professor?tid=846287")  # Rabinovich,Michael
+add_to_data("https://www.ratemyprofessors.com/professor?tid=1868954")  # Ray,Soumya
 # Shkurti,Thomas
-addToData("https://www.ratemyprofessors.com/professor?tid=321409")  # Szarek,Stanislaw
-addToData("https://www.ratemyprofessors.com/professor?tid=935633")  # Thomas,Peter
-addToData("https://www.ratemyprofessors.com/professor?tid=2425127")  # Wang,An
-# addToData("https://www.ratemyprofessors.com/professor?tid=2764934") # Weyer,Daniel
-# addToData("https://www.ratemyprofessors.com/professor?tid=2025941") # Wilson,David
-addToData("https://www.ratemyprofessors.com/professor?tid=2558896")  # Wu,Yinghui
-addToData("https://www.ratemyprofessors.com/professor?tid=2355255")  # Xiao,Xusheng
+add_to_data("https://www.ratemyprofessors.com/professor?tid=321409")  # Szarek,Stanislaw
+add_to_data("https://www.ratemyprofessors.com/professor?tid=935633")  # Thomas,Peter
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2425127")  # Wang,An
+# add_to_data("https://www.ratemyprofessors.com/professor?tid=2764934") # Weyer,Daniel
+# add_to_data("https://www.ratemyprofessors.com/professor?tid=2025941") # Wilson,David
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2558896")  # Wu,Yinghui
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2355255")  # Xiao,Xusheng
 # Xu,Kevin
-addToData("https://www.ratemyprofessors.com/professor?tid=2519044")  # Xu,Shuai
-addToData("https://www.ratemyprofessors.com/professor?tid=2524332")  # Ye,Yanfang
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2519044")  # Xu,Shuai
+add_to_data("https://www.ratemyprofessors.com/professor?tid=2524332")  # Ye,Yanfang
 
-printTable()
+print_table()
