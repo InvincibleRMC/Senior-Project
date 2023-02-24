0b07663f0743988b5d44c52d81e505866398cfff
Michael Carlstrom <rmc170@case.edu>
2023-02-23 18:03:33 -0800

    started adding flutter helper functions

--- front_end/lib/network.dart
+++ front_end/lib/network.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
+import 'package:front_end/proto/data.pbserver.dart';
 import 'package:tcp_socket_connection/tcp_socket_connection.dart';
 
 import 'package:protobuf/protobuf.dart';
@@ -8,10 +10,13 @@ import 'package:front_end/proto/requests.pb.dart';
 import 'package:front_end/proto/responses.pb.dart';
 
 class Network {
-  Utf8Decoder _decoder = Utf8Decoder();
+  final Utf8Decoder _decoder = const Utf8Decoder();
   TcpSocketConnection? _connection;
 
   static final Network _instance = Network._internal();
+  List<Course>? classes;
+  List<Professor>? professors;
+  // List<String>? majors;
 
   // using a factory is important
   // because it promises to return _an_ object of this type
@@ -30,45 +35,84 @@ class Network {
   }
 
   void messageReceived(String message) {
-    // print(message);
-
     var res = Response();
     res.clear();
 
     var read = CodedBufferReader(message.codeUnits);
     res.mergeFromCodedBufferReader(read);
 
-    print(res.toDebugString());
-    print(res.r4.msg);
+    switch (res.type) {
+      case ResponseType.RES_DEBUG:
+        {
+          print("DEBUG recieved");
+        }
+        break;
+      case ResponseType.RES_COURSES:
+        {
+          classes = res.r3.courses;
+        }
+        break;
+      case ResponseType.RES_PROFS:
+        {
+          professors = res.r2.professors;
+        }
+        break;
+      case ResponseType.RES_SCHEDULE:
+        {
+          // TODO: schedule object
+          // schedule = res.r1.;
+        }
+        break;
+    }
 
     var req = Request();
     req.type = RequestType.REQ_DEBUG;
     req.r5 = DebugRequest();
     req.r5.msg = "ping";
 
-    _connection?.sendMessage(req.toString());
+    Uint8List out = req.writeToBuffer();
+    _connection?.sendMessage(_decoder.convert(out));
   }
 
   void startConnection() async {
-    _connection?.enableConsolePrint(
+    _connection!.enableConsolePrint(
         true); //use this to see in the console what's happening
 
-    if (_connection != null) {
-      if (await _connection!.canConnect(50000, attempts: 100000)) {
-        //check if it's possible to connect to the endpoint
-        await _connection?.connect(50000, messageReceived, attempts: 10000000);
-      }
-
-      var req = Request();
-      req.clear();
-
-      req.type = RequestType.REQ_DEBUG;
-      req.r5 = DebugRequest();
-      req.r5.msg = "ping";
-
-      var out = req.writeToBuffer();
-
-      _connection!.sendMessage(_decoder.convert(out));
+    if (await _connection!.canConnect(50000, attempts: 100000)) {
+      //check if it's possible to connect to the endpoint
+      await _connection!.connect(50000, messageReceived, attempts: 10000000);
     }
+
+    var req = Request();
+    req.clear();
+
+    req.type = RequestType.REQ_DEBUG;
+    req.r5 = DebugRequest();
+    req.r5.msg = "ping";
+
+    Uint8List out = req.writeToBuffer();
+
+    _connection!.sendMessage(_decoder.convert(out));
+  }
+
+  void requestHelper(Request req) {
+    Uint8List out = req.writeToBuffer();
+    _connection!.sendMessage(_decoder.convert(out));
+  }
+
+  // TODO: waiting for ENUM
+  void sendProfessorRequest(String major) {
+    var req = Request();
+
+    req.r3 = ProfessorRequest();
+    req.r3.major = major;
+    requestHelper(req);
+  }
+
+  // TODO: waiting for ENUM
+  void sendClassRequest() {
+    var req = Request();
+
+    requestHelper(req);
   }
 }
