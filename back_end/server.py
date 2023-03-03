9252858989493333e7c5ede48aa564bb8974b5c8
Michael Carlstrom <rmc170@case.edu>
2023-03-02 20:09:52 -0800

    id fix

--- back_end/server.py
+++ back_end/server.py
@@ -8,7 +8,8 @@ from sqlite3 import Connection, Cursor
 import sqlite3
 from typing import List, Tuple
 
-from proto.requests_pb2 import Request, DebugRequest, REQ_DEBUG, REQ_COURSE, REQ_NOTIFICATION, REQ_SCHEDULE, REQ_PROFESSOR
+from proto.requests_pb2 import Request, REQ_DEBUG, REQ_COURSE, REQ_NOTIFICATION
+from proto.requests_pb2 import REQ_SCHEDULE, REQ_PROFESSOR
 from proto.responses_pb2 import Response, DebugResponse, RES_DEBUG
 
 from proto.requests_pb2 import *
@@ -73,6 +74,7 @@ class WorkerThread:
                 continue
 
             (conn, addr, request) = self.rx_q.get_nowait()
+            addr = addr
             # self.rx_q.task_done()
             self.rx_lock.release()
 
@@ -107,10 +109,10 @@ class WorkerThread:
     def handle_req_course(self, req: CourseRequest) -> CourseResponse:
         res = CourseResponse()
 
-        def create_course(id: int, name: str, semester: str) -> Course:
+        def create_course(ident: int, name: str, semester: str) -> Course:
             course = Course()
             course.Clear()
-            course.id = id
+            course.id = ident
             course.name = name
             course.semester = semester
             return course
@@ -121,11 +123,11 @@ class WorkerThread:
     def handle_req_prof(self, req: ProfessorRequest) -> ProfessorResponse:
         res = ProfessorResponse()
 
-        def create_prof(id: int, first: str, last: str) -> Professor:
+        def create_prof(ident: int, first: str, last: str) -> Professor:
             prof = Professor()
             prof.Clear()
-            prof.id = id 
-            prof.first = first 
+            prof.id = ident
+            prof.first = first
             prof.last = last
             return prof
 
@@ -140,10 +142,10 @@ class WorkerThread:
 
     def handle_req_major(self, req: MajorRequest) -> MajorResponse:
 
-        def create_major(id: int, name: str) -> Major:
+        def create_major(ident: int, name: str) -> Major:
             major = Major()
             major.Clear()
-            major.id = id
+            major.id = ident
             major.name = name
             return major
 
