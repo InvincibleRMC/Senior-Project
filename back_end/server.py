01533b24818403e28833e753653d414b17e7ccc0
Michael Carlstrom <rmc170@case.edu>
2023-03-09 07:42:00 -0800

    added more types to server.py

--- back_end/server.py
+++ back_end/server.py
@@ -5,6 +5,8 @@ import signal
 
 from concurrent.futures import ThreadPoolExecutor
 
+from typing import Dict
+
 import grpc
 from proto.service_pb2_grpc import ServiceServicer, add_ServiceServicer_to_server
 
@@ -17,6 +19,7 @@ from proto.requests_pb2 import (MajorRequest,
 from proto.responses_pb2 import (CourseList, DebugResponse,MajorResponse,
                                  NotificationResponse, ScheduleResponse, CourseResponse,
                                  ProfessorResponse)
+
 
 class Service(ServiceServicer):
 
@@ -35,35 +38,17 @@ class Service(ServiceServicer):
         course.semester = semester
         return course
 
-    def GetSchedule(self, request, context):
+    def GetSchedule(self, request, context) -> ScheduleResponse:
         print(f"Received Schedule request: {repr(request)}")
         res = ScheduleResponse()
         # TODO:
-        res.course_map = {}
+        res.course_map: Dict[string, CourseList] = {}
         res.course_map["fall1"] = CourseList(courses=[])
-
-        # res.course_map.extend([self.create_course(1, "bruh", "spring"),
-        #                     self.create_course(2, "yeet", "fall")])
-        # res.spring1.extend([self.create_course(1, "bruh", "spring"),
-        #                     self.create_course(2, "yeet", "fall")])
-        # res.fall2.extend([self.create_course(1, "bruh", "spring"),
-        #                     self.create_course(2, "yeet", "fall")])
-        # res.spring2.extend([self.create_course(1, "bruh", "spring"),
-        #                     self.create_course(2, "yeet", "fall")])
-        # res.fall3.extend([self.create_course(1, "bruh", "spring"),
-        #                     self.create_course(2, "yeet", "fall")])
-        # res.spring3.extend([self.create_course(1, "bruh", "spring"),
-        #                     self.create_course(2, "yeet", "fall")])
-        # res.fall4.extend([self.create_course(1, "bruh", "spring"),
-        #                     self.create_course(2, "yeet", "fall")])
-        # res.spring4.extend([self.create_course(1, "bruh", "spring"),
-        #                     self.create_course(2, "yeet", "fall")])
         return res
-    
-    def RegisterNotifications(self, request, context):
+
+    def RegisterNotifications(self, request, context) -> NotificationResponse:
         print(f"Received notification registration: {repr(request)}")
-        return NotificationResponse(True)
-    
+        return NotificationResponse(True)  
 
     @staticmethod
     def create_prof(ident: int, first: str, last: str) -> Professor:
@@ -75,16 +60,15 @@ class Service(ServiceServicer):
         prof.last = last
         return prof
 
-    def GetProfessors(self, request, context):
+    def GetProfessors(self, request, context) -> ProfessorResponse:
         """Generates ProfessorResponse from ProfessorRequest"""
         print(f"Received professor request:", request)
         res = ProfessorResponse()
         res.professors.extend([self.create_prof(1, "Ronald", "Loui"),
                                self.create_prof(2, "Harold", "Connamacher")])
         return res
-        # return super().GetProfessors(request, context)
-    
-    def GetCourses(self, request, context):
+
+    def GetCourses(self, request, context) -> CourseResponse:
         print(f"Received course request: {repr(request)}")
         res = CourseResponse()
         res.courses.extend([self.create_course(1, "bruh", "spring"),
@@ -100,13 +84,13 @@ class Service(ServiceServicer):
         major.name = name
         return major
 
-    def GetMajors(self, request, context):
+    def GetMajors(self, request, context) -> MajorResponse:
         print(f"Received major request: {repr(request)}")
         res = MajorResponse()
         res.majors.extend([self.create_major(1, "CS"), self.create_major(2, "CE")])
         return res
 
-    def Debug(self, request, context):
+    def Debug(self, request, context) -> DebugResponse:
         print(f"Received {request.msg}")
         return DebugResponse(msg="pong")
 
